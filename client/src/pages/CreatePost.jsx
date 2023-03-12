0c1b88109b9c5a589791e5ae70d197c628df1259
LokosFero <bilardoraka64@gmail.com>
2023-03-12 22:06:10 +0700

    change Server

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,151 +1,172 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { preview } from '../assets';
-import { getRandomPrompt } from '../utils';
-import { Loader, FormField } from '../components';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { preview } from "../assets";
+import { getRandomPrompt } from "../utils";
+import { Loader, FormField } from "../components";
 
 const CreatePost = () => {
   const navigate = useNavigate();
   const [form, setform] = useState({
-    name:'', 
-    prompt:'',
-    photo:''
+    name: "",
+    prompt: "",
+    photo: "",
   });
 
   const [generatingImg, setgeneratingImg] = useState(false);
-  const [loading, setloading] = useState(false); 
+  const [loading, setloading] = useState(false);
 
   const generateImage = async () => {
-    if(form.prompt) {
+    if (form.prompt) {
       try {
         setgeneratingImg(true);
-        const response = await fetch('https://brpdalle.herokuapp.com/api/v1/dalle', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ prompt: form.prompt }),
-        });
+        const response = await fetch(
+          "https://serverdalle-bilardoraka64.b4a.run/api/v1/dalle",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ prompt: form.prompt }),
+          }
+        );
 
         const data = await response.json();
 
-        setform({ ...form, photo: `data:image/jpeg;base64,${data.photo}` })
+        setform({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
       } catch (error) {
         alert(error);
       } finally {
         setgeneratingImg(false);
       }
     } else {
-      alert('Masukkan prompt terlebih dahulu.')
+      alert("Masukkan prompt terlebih dahulu.");
     }
-  }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if(form.prompt && form.photo) {
+    if (form.prompt && form.photo) {
       setloading(true);
 
       try {
-        const response = await fetch("https://brpdalle.herokuapp.com/api/v1/post",{
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(form)
-        });
+        const response = await fetch(
+          "https://brpdalle.herokuapp.com/api/v1/post",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(form),
+          }
+        );
 
         await response.json();
-        navigate('/');
+        navigate("/");
       } catch (error) {
-        alert(error)
+        alert(error);
       } finally {
         setloading(false);
       }
     } else {
-      alert('Silahkan masukkan prompt.')
+      alert("Silahkan masukkan prompt.");
     }
-  }
+  };
 
   const handleChange = (e) => {
-    setform({ ...form, [e.target.name]: e.target.value })
-  }
+    setform({ ...form, [e.target.name]: e.target.value });
+  };
 
   const handleSurpriseMe = () => {
     const randomPrompt = getRandomPrompt(form.prompt);
-    setform({ ...form, prompt: randomPrompt })
-  }
+    setform({ ...form, prompt: randomPrompt });
+  };
 
   return (
-    <section className='max-w-7xl mx-auto'>
+    <section className="max-w-7xl mx-auto">
       <div>
-        <h1 className='font-extrabold text-[#222328] text-[32px]'>Buat Gambar</h1>
-        <p className='mt-2 text-[#666E75] text-[16px] max-w-[500px]'>Membuat gambar yang imaginatif dengan visual yang mengagumkan di Dall-E AI Clone dari <a target='_blank' className='text-[#6469FF] underline' href='https://github.com/BilardoRaka'>BRPamungkas</a></p>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">
+          Buat Gambar
+        </h1>
+        <p className="mt-2 text-[#666E75] text-[16px] max-w-[500px]">
+          Membuat gambar yang imaginatif dengan visual yang mengagumkan di
+          Dall-E AI Clone dari{" "}
+          <a
+            target="_blank"
+            className="text-[#6469FF] underline"
+            href="https://github.com/BilardoRaka"
+          >
+            BRPamungkas
+          </a>
+        </p>
       </div>
-      <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
-        <div className='flex flex-col gap-5'> 
-          <FormField 
-            labelName='Nama Anda' 
-            type='text' 
-            name='name' 
-            placeholder='Joe Mama' 
-            value={form.name} 
-            handleChange={handleChange} 
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
+          <FormField
+            labelName="Nama Anda"
+            type="text"
+            name="name"
+            placeholder="Joe Mama"
+            value={form.name}
+            handleChange={handleChange}
           />
-          <FormField 
-            labelName='Prompt' 
-            type='text' 
-            name='prompt' 
-            placeholder='Two futuristic towers with a skybridge covered in lush foliage, digital art' 
-            value={form.prompt} 
-            handleChange={handleChange} 
+          <FormField
+            labelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="Two futuristic towers with a skybridge covered in lush foliage, digital art"
+            value={form.prompt}
+            handleChange={handleChange}
             isSurpriseMe
             handleSurpriseMe={handleSurpriseMe}
           />
 
-          <div className='relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center'>
+          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
             {form.photo ? (
-              <img 
+              <img
                 src={form.photo}
                 alt={form.prompt}
-                className='w-full h-full object-contain'
+                className="w-full h-full object-contain"
               />
             ) : (
-              <img 
+              <img
                 src={preview}
                 alt="Preview"
-                className='w-9/12 h-9/12 object-contain opacity-40'
+                className="w-9/12 h-9/12 object-contain opacity-40"
               />
             )}
 
             {generatingImg && (
-              <div className='absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg'>
+              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
                 <Loader />
               </div>
             )}
           </div>
         </div>
-        <div className='mt-5 flex gap-5'>
+        <div className="mt-5 flex gap-5">
           <button
-            type='button'
+            type="button"
             onClick={generateImage}
-            className='text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
+            className="text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
           >
-            {generatingImg ? 'Generating...' : 'Generate'}
+            {generatingImg ? "Generating..." : "Generate"}
           </button>
         </div>
-        <div className='mt-10'>
-          <p className='mt-2 text-[#666E75] text-[16px]'>Setelah membuat gambar yang kamu mau, kamu bisa share ke semua orang di komunitas.</p>
+        <div className="mt-10">
+          <p className="mt-2 text-[#666E75] text-[16px]">
+            Setelah membuat gambar yang kamu mau, kamu bisa share ke semua orang
+            di komunitas.
+          </p>
           <button
-            type='submit'
-            className='mt-3 text-white bg-[#6469FF] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
+            type="submit"
+            className="mt-3 text-white bg-[#6469FF] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
           >
-            {loading ? 'Sharing...' : 'Share ke komunitas'}
+            {loading ? "Sharing..." : "Share ke komunitas"}
           </button>
-        </div>    
+        </div>
       </form>
     </section>
-  )
-}
+  );
+};
 
-export default CreatePost
+export default CreatePost;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { Card, FormField, Loader } from '../components';
+import React, { useState, useEffect } from "react";
+import { Card, FormField, Loader } from "../components";
 
 const RenderCards = ({ data, title }) => {
   if (data?.length > 0) {
-    return (
-      data.map((post) => <Card key={post._id} {...post} />)
-    );
+    return data.map((post) => <Card key={post._id} {...post} />);
   }
 
   return (
@@ -13,12 +11,11 @@ const RenderCards = ({ data, title }) => {
   );
 };
 
-
 const Home = () => {
   const [loading, setloading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
 
-  const [searchText, setsearchText] = useState('');
+  const [searchText, setsearchText] = useState("");
   const [searchedResult, setsearchedResult] = useState(null);
   const [searchTimeout, setsearchTimeout] = useState(null);
 
@@ -26,12 +23,15 @@ const Home = () => {
     setloading(true);
 
     try {
-      const response = await fetch("https://brpdalle.herokuapp.com/api/v1/post", {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+      const response = await fetch(
+        "https://serverdalle-bilardoraka64.b4a.run/api/v1/post",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
       if (response.ok) {
         const result = await response.json();
@@ -53,60 +53,73 @@ const Home = () => {
 
     setsearchTimeout(
       setTimeout(() => {
-        const searchResults = allPosts.filter((item) => item.name.toLowerCase().includes(searchText.toLowerCase()) || item.prompt.toLowerCase().includes(searchText.toLowerCase()));
-  
+        const searchResults = allPosts.filter(
+          (item) =>
+            item.name.toLowerCase().includes(searchText.toLowerCase()) ||
+            item.prompt.toLowerCase().includes(searchText.toLowerCase())
+        );
+
         setsearchedResult(searchResults);
       }, 500)
     );
-  }
-  
+  };
+
   return (
-    <section className='max-w-7xl mx-auto'>
+    <section className="max-w-7xl mx-auto">
       <div>
-        <h1 className='font-extrabold text-[#222328] text-[32px]'>The Community Showcase</h1>
-        <p className='mt-2 text-[#666E75] text-[16px] max-w-[500px]'>Berjalan menuju koleksi foto yang imaginatif dengan visual yang mengagumkan di Dall-E AI Clone by <a target='_blank' className='text-[#6469FF] underline' href='https://github.com/BilardoRaka'>BRPamungkas</a></p>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">
+          The Community Showcase
+        </h1>
+        <p className="mt-2 text-[#666E75] text-[16px] max-w-[500px]">
+          Berjalan menuju koleksi foto yang imaginatif dengan visual yang
+          mengagumkan di Dall-E AI Clone by{" "}
+          <a
+            target="_blank"
+            className="text-[#6469FF] underline"
+            href="https://github.com/BilardoRaka"
+          >
+            BRPamungkas
+          </a>
+        </p>
       </div>
-      <div className='mt-16'>
-        <FormField 
-          labelName='Search'
-          type='text'
-          name='text'
-          placeholder='Pencarian Gambar'
+      <div className="mt-16">
+        <FormField
+          labelName="Search"
+          type="text"
+          name="text"
+          placeholder="Pencarian Gambar"
           value={searchText}
           handleChange={handleSearchChange}
         />
       </div>
-      <div className='mt-10'>
+      <div className="mt-10">
         {loading ? (
-          <div className='flex justify-center items-center'>
+          <div className="flex justify-center items-center">
             <Loader />
           </div>
         ) : (
           <>
             {searchText && (
-              <h2 className='font-medium text-[#666E75] text-xl mb-3'>
-                Menampilkan hasil untuk <span className='text-[#222328]'>{searchText}</span>
+              <h2 className="font-medium text-[#666E75] text-xl mb-3">
+                Menampilkan hasil untuk{" "}
+                <span className="text-[#222328]">{searchText}</span>
               </h2>
-            )}  
-            <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grind-cols-1 gap-3'>
+            )}
+            <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grind-cols-1 gap-3">
               {searchText ? (
-                <RenderCards 
-                  data={searchedResult} 
-                  title='Hasil pencarian tidak ditemukan.' 
+                <RenderCards
+                  data={searchedResult}
+                  title="Hasil pencarian tidak ditemukan."
                 />
               ) : (
-                <RenderCards 
-                  data={allPosts} 
-                  title='Tidak ada gambar.' 
-                />
+                <RenderCards data={allPosts} title="Tidak ada gambar." />
               )}
             </div>
           </>
-        )
-      }
+        )}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
